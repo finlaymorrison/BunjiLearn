076f533ebd3c34c498763f86c943d6348b24e4f4
Finlay Morrison <morrison.fin02@gmail.com>
2022-09-07 21:39:02 +0100

    Make use of val+test datasets and batch training.

--- bunjilearn/model/include/trainer.hpp
+++ bunjilearn/model/include/trainer.hpp
@@ -8,6 +8,13 @@
 namespace bunji
 {
 
+enum class DatasetPartition
+{
+    Train = 0,
+    Validation = 1,
+    Test = 2
+};
+
 class Trainer
 {
 private:
@@ -18,9 +25,10 @@ private:
     double learn_rate;
 public:
     Trainer(Network *network, Dataset *dataset, Loss *loss, const std::vector<Metric*> &metrics, double learn_rate=0.001);
-    void train_example(const Tensor<double, 3> &input, const Tensor<double, 3> &expected_output);
-    std::vector<double> train_pass();
-    void fit(int epochs);
+    void process_example(const Tensor<double, 3> &input, const Tensor<double, 3> &expected_output, bool training);
+    void dataset_pass(DatasetPartition partition, std::size_t dataset_len, std::size_t metrics, bool training);
+    void evaluate_metrics(const std::string &part_name, std::size_t dataset_len);
+    void fit(std::size_t epochs, std::size_t batch_size);
 };
 
 } // namespace bunji
